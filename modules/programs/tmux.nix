d8e529877acdab801de2881999f1da9667160383
Elvar <m@martinelvar.dk>
2025-08-11 12:50:05 +0200

    tmux

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -2,19 +2,29 @@
 
 {
   programs.tmux = {
-    enabled = true;
+    enable = true;
     mouse = true;
     baseIndex = 1;
     shortcut = "Space";
+    extraConfig = ''
+    unbind r
+    bind r source-file ~/.tmux.conf
+
+    bind-key h select-pane -L
+    bind-key l select-pane -R
+    bind-key j select-pane -D
+    bind-key k select-pane -U
+    '';
     plugins = with pkgs; [
       tmuxPlugins.sensible
       tmuxPlugins.yank
-      tmuxPlugins.tmux-fingers
+      tmuxPlugins.fingers
       tmuxPlugins.vim-tmux-navigator
       tmuxPlugins.cpu
       {
         plugin = tmuxPlugins.catppuccin;
         extraConfig = ''
+          set-option -g status-position top
           set -g @catppuccin_flavor 'mocha'
           set -g @catppuccin_window_status_style 'rounded'
           set -g status-right-length 100
@@ -22,13 +32,8 @@
           set -g status-left ""
           set -g status-right "#{E:@catppuccin_status_application}"
           set -ag status-right "#{E:@catppuccin_status_session}"
-          set -ag status-right "#{E:@catppuccin_status_uptime}"
         '';
       }
     ];
-    extraConfigBeforePlugins = ''
-      # Enable color
-      set-option -sa terminal-overrides ",xterm*:Tc"
-    '';
   };
 }
